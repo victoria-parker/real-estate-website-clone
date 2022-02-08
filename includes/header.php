2aa8db27aa9a5f43c66580709a4280be67825ef6
victoria-parker <victoriaparkerb@gmail.com>
2022-02-08 23:03:32 +0000

    first commit

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Real Estate Website Clone</title>
+</head>
+<body>
+    <header>
+        <div class="social-media">
+            <span>01 123 4567</span>
+            <ul>
+                <li><a href="https://www.facebook.com" target="_blank" title="Facebook"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="https://www.linkedin.com" target="_blank" title="LinkedIn"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://www.twitter.com" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://www.instagram.com" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+        <div id="navigation-bar">
+            <div id="logo"><img src="images/logo.png" alt="logo" width="100px"></div>
+            <nav id="menu">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SALES</a></li>
+                    <li><a href="#">LETTING</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    include 'includes/header.php';
+?>
+<main>
+<div class="hero-background hero-index">
+    <div>
+        <div>
+            <h1>We Provide The Key To Successful 
+            <br>    
+            <strong>Sales, Lettings & Property Management</strong></h1>
+            <h2>Real Estate Agents Dublin: All You Need to Know Before Selling Your Property</h2>
+        </div>
+        <a href="#" class="call-to-action">Explore our properties</a>  
+    </div>
+
+</div>
+    <section id="our-services">
+        <div><i class="fal fa-home our-sv-img"></i><h2>Let Properties</h2><h3>No let, No fee!</h3><a href="#" class="sm-call-to-action">Explore</a></div>
+        <div><i class="fal fa-key-skeleton our-sv-img"></i><h2>Sell Properties</h2><h3>No sell, No fee!</h3><a href="#" class="sm-call-to-action">Explore</a></div>
+        <div><i class="fal fa-tasks our-sv-img"></i><h2>Property Management</h2><h3>Because we care</h3><a href="#" class="sm-call-to-action">Explore</a></div>
+        <div><i class="fal fa-comment-alt-dollar our-sv-img"></i><h2>Property Valuation</h2><h3>The value you deserve</h3><a href="#" class="sm-call-to-action">Explore</a></div>
+    </section>
+</main>
+
+<?php
+    include 'includes/footer.php';
+?>
