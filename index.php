f3f727f0f6a27259f8b43b787fa30ed015dd6df2
victoria-parker <victoriaparkerb@gmail.com>
2022-02-08 23:34:20 +0000

    search form added

--- index.php
+++ index.php
@@ -20,6 +20,36 @@
         <div><i class="fal fa-tasks our-sv-img"></i><h2>Property Management</h2><h3>Because we care</h3><a href="#" class="sm-call-to-action">Explore</a></div>
         <div><i class="fal fa-comment-alt-dollar our-sv-img"></i><h2>Property Valuation</h2><h3>The value you deserve</h3><a href="#" class="sm-call-to-action">Explore</a></div>
     </section>
+    <section id="search-property">
+        <h2>Search our available properties</h2>
+        <form action="" method="GET" id="form-search">
+            <div>
+                <label for="type">TYPE</label><br>
+                <select name="type" id="type">
+                    <option value="type">Type</option>
+                </select>
+            </div>
+            <div>
+                <label for="location">Location</label><br>
+                <select name="location" id="location">
+                    <option value="location">location</option>
+                </select>
+            </div>
+            <div>
+                <label for="max-bedrooms">max-bedrooms</label><br>
+                <select name="max-bedrooms" id="max-bedrooms">
+                    <option value="max-bedrooms">max-bedrooms</option>
+                </select>
+            </div>
+            <div>
+                <label for="max-price">max-price</label><br>
+                <select name="max-price" id="max-price">
+                    <option value="max-price">max-price</option>
+                </select>
+            </div>
+            <button class="sm-call-to-action">Search</button>
+        </form>
+    </section>
 </main>
 
 <?php
