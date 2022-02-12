5699e1a8ba04ae7616596767640626ae92e24f9e
victoria-parker <victoriaparkerb@gmail.com>
2022-02-12 15:14:37 +0000

    footer added and logo changed

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,40 @@
+<footer>
+    <div>
+        <h3>contact us</h3> 
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div>
+        <h3>estate agents</h3>
+         <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div>
+        <h3>irish property news</h3> 
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div>
+        <h3>follow us</h3> 
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</footer>
 </body>
 </html>
