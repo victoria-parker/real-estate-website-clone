6f218c042ee6d0f4004d59ab6f499d65eebf5c6d
victoria-parker <victoriaparkerb@gmail.com>
2022-02-12 15:49:21 +0000

    footer completed and styled

--- includes/footer.php
+++ includes/footer.php
@@ -1,38 +1,36 @@
 <footer>
     <div>
-        <h3>contact us</h3> 
+        <h3>Contact us</h3> 
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="#">1 George Road News, Drogheda </a></li>
+            <li><a href="#">01 123 4567</a></li>
+            <li><a href="#">Email: info@fake.com</a></li>
         </ul>
     </div>
     <div>
-        <h3>estate agents</h3>
+        <h3>Estate agents</h3>
          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="#">Property Management</a></li>
+            <li><a href="#">Property Letting</a></li>
+            <li><a href="#">Property Sales</a></li>
+            <li><a href="#">Property Valuations</a></li>
         </ul>
     </div>
     <div>
-        <h3>irish property news</h3> 
+        <h3>Property Advice</h3> 
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="#">Why hiring a Estate Agent will benefit you</a></li>
+            <li><a href="#">Is this the best time to sell?</a></li>
+            <li><a href="#">Why Do Landlords Hire a Estate Agent?</a></li>
         </ul>
     </div>
     <div>
-        <h3>follow us</h3> 
+        <h3>Follow us</h3> 
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="https://www.facebook.com" target="_blank" title="Facebook"><i class="fab fa-facebook"></i>Facebook</a></li>
+            <li><a href="https://www.linkedin.com" target="_blank" title="LinkedIn"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+            <li><a href="https://www.twitter.com" target="_blank" title="Twitter"><i class="fab fa-twitter"></i>Twitter</a></li>
+            <li><a href="https://www.instagram.com" target="_blank" title="Instagram"><i class="fab fa-instagram"></i>Instagram</a></li>
         </ul>
     </div>
 </footer>
