a30009323cc285197e8b0aecfdec5925e95730d4
victoria-parker <victoriaparkerb@gmail.com>
2022-02-16 21:20:59 +0000

    index links completed

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                 <strong>Sales, Lettings & Property Management</strong></h1>
                 <h2>Real Estate Agents Drogheda: All You Need to Know Before Selling Your Property</h2>
             </div>
-            <a href="#" class="call-to-action">Explore our properties</a>  
+            <a href="#featured-properties" class="call-to-action">Explore our properties</a>  
         </div>
 
     </div>
@@ -22,10 +22,10 @@
 
 
     <section id="our-services">
-        <div><i class="fal fa-home our-sv-img"></i><h2>Let Properties</h2><h3>No let, No fee!</h3><a href="#" class="sm-call-to-action">Explore</a></div>
-        <div><i class="fal fa-key-skeleton our-sv-img"></i><h2>Sell Properties</h2><h3>No sell, No fee!</h3><a href="#" class="sm-call-to-action">Explore</a></div>
-        <div><i class="fal fa-tasks our-sv-img"></i><h2>Property Management</h2><h3>Because we care</h3><a href="#" class="sm-call-to-action">Explore</a></div>
-        <div><i class="fal fa-comment-alt-dollar our-sv-img"></i><h2>Property Valuation</h2><h3>The value you deserve</h3><a href="#" class="sm-call-to-action">Explore</a></div>
+        <div><i class="fal fa-home our-sv-img"></i><h2>Let Properties</h2><h3>No let, No fee!</h3><a href="contact.php" class="sm-call-to-action">Contact our agents</a></div>
+        <div><i class="fal fa-key-skeleton our-sv-img"></i><h2>Sell Properties</h2><h3>No sell, No fee!</h3><a href="contact.php" class="sm-call-to-action">Contact our agents</a></div>
+        <div><i class="fal fa-tasks our-sv-img"></i><h2>Property Management</h2><h3>Because we care</h3><a href="contact.php" class="sm-call-to-action">Contact our agents</a></div>
+        <div><i class="fal fa-comment-alt-dollar our-sv-img"></i><h2>Property Valuation</h2><h3>The value you deserve</h3><a href="contact.php" class="sm-call-to-action">Contact our agents</a></div>
     </section>
 
 
@@ -64,7 +64,7 @@
 
 
 
-    <section class="featured-properties">
+    <section class="featured-properties" id="featured-properties">
         <h2>Featured Sale Properties</h2>
         <div class="fp-container">
 
@@ -90,6 +90,7 @@
         }
         ?>
         </div>
+        <a href="sales.php" class="sm-call-to-action">Explore sales</a>
 
 
         <h2>Featured Rent Properties</h2>
@@ -114,6 +115,7 @@
             }
             ?>
         </div>
+        <a href="#featured-properties" class="sm-call-to-action">Explore Lettings</a>
     </section>
 
 
@@ -122,7 +124,7 @@
             <span>THINKING TO SELL?</span>
             <h2>Selling your Property?</h2>
             <p>We believe that the real estate world should be very simple. Thanks to our technology and our team, we are able to buy homes in just 7 days, renovate them and put them up for sale again. A completely new experience whether you buy or sell.</p>
-            <a href="#" class="sm-call-to-action">contact our agents</a>
+            <a href="contact.php" class="sm-call-to-action">contact our agents</a>
         </div>
         <div class="articles-img">
             <img src="images/moving-house.jpg" alt="family moving">
@@ -138,7 +140,7 @@
             <span>THINKING TO LET?</span>
             <h2>Letting Your Property?</h2>
             <p>Don't worry about collecting the rent for your home. Minimize the risks when renting. We make a careful selection of the tenant, and we study their solvency, employment contract, payroll, and delinquency. We do a commercial management of your home, providing truthful and objective information, being very efficient and demanding with our results.</p>
-            <a href="#" class="sm-call-to-action">contact our agents</a>
+            <a href="contact.php" class="sm-call-to-action">contact our agents</a>
         </div>
     </section>
 
